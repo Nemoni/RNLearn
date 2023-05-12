c3381b6c1256269b522f86779295d03b26da3e01
Nemoni <490202694@qq.com>
2023-05-12 16:41:15 +0800

    FlatList显示数据

--- Test.tsx
+++ Test.tsx
@@ -1,83 +1,67 @@
-// 题目1：创建一个简单的登录表单，包括用户名、密码输入框和登录按钮。实现输入验证功能，如非空验证、电子邮件格式验证等。
-import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  Alert,
-} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native';
+import React from 'react';
 
-const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Data = [
+  {
+    id: 1,
+    title: 'First Item',
+  },
+  {
+    id: 2,
+    title: 'Second Item',
+  },
+  {
+    id: 3,
+    title: 'Third Item',
+  },
+];
 
-  const validateEmail = email => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
+const renderItem = ({item}) => (
+  <View style={styles.item}>
+    <Text style={styles.text}>{item.title}</Text>
+  </View>
+);
 
-  const handleLogin = () => {
-    if (!email || !password) {
-      Alert.alert('错误', '请填写所有字段');
-    } else if (!validateEmail(email)) {
-      Alert.alert('错误', '请输入有效的电子邮件地址');
-    } else {
-      // 进行登录操作
-    }
-  };
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="电子邮件"
-        onChangeText={setEmail}
-        value={email}
-        keyboardType="email-address"
-        autoCapitalize="none"
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={Data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        contentContainerStyle={styles.flatlist}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="密码"
-        onChangeText={setPassword}
-        value={password}
-        secureTextEntry
-      />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>登录</Text>
-      </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    // backgroundColor: 'yellow',
+  },
+  flatlist: {
+    flex: 1,
+    // backgroundColor: 'red',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  input: {
-    width: '80%',
+  item: {
+    backgroundColor: 'pink',
+    borderwidth: 1,
+    borderColor: 'black',
+    width: 200,
+    height: 30,
+  },
+  text: {
+    flex: 1,
+    color: 'black',
+    textAlign: 'center',
+    textAlignVertical: 'center',
     borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    marginBottom: 10,
-  },
-  button: {
-    backgroundColor: '#3498db',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: '#FFF',
-    fontWeight: 'bold',
+    borderColor: 'black',
   },
 });
-
-export default LoginForm;
