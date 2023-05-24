a212aacd941a8d05602a29fa51a58a1ba1364cde
Nemoni <490202694@qq.com>
2023-05-24 22:17:00 +0800

    通过native module访问android代码 英文版官网例子

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,10 +1,20 @@
-import {Text, View} from 'react-native';
 import React from 'react';
+import {NativeModules, Button} from 'react-native';
 
-export default function App() {
+const NewModuleButton = () => {
+  const {CalendarModule} = NativeModules;
+  const onPress = () => {
+    console.log('We will invoke the native module here!');
+    CalendarModule.createCalendarEvent('testName', 'testLocation');
+  };
+
   return (
-    <View>
-      <Text>Hello world</Text>
-    </View>
+    <Button
+      title="Click to invoke your native module!"
+      color="#841584"
+      onPress={onPress}
+    />
   );
-}
+};
+
+export default NewModuleButton;
